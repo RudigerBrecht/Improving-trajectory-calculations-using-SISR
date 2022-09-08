23b50c9eeb894429de27948feefef6fd2835870e
RudigerBrecht <rbrecht@mun.ca>
2022-09-08 13:25:02 +0200

    Update normalize_data.py

--- preprocess_data/normalize_data.py
+++ preprocess_data/normalize_data.py
@@ -10,92 +10,94 @@ def mynormalize(x):
     return 2*(x-np.min(x))/(np.max(x)-np.min(x))-1
 ###
 
-udata=np.zeros((87,360,720,75*4),dtype=np.float32)
+udata=np.zeros((137,360,720,75*3),dtype=np.float32)
 
-ds = nc.Dataset('../download_era5_data/train_u.nc')
-u1=ds['u'][50:137,0:360,0:720,0:75*4]
+ds = nc.Dataset('wind_orig_20000101000000.nc')
+u1=ds['u'][0:137,0:360,0:720,0:75]
 
-for i in range(87):
+for i in range(137):
     for j in range(75):
         u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
 
 udata[:,:,:,0:75]=u1
 
-np.save('normalized_train_u_50_137',udata)
+ds = nc.Dataset('wind_orig_20000104050000.nc')
+u1=ds['u'][0:137,0:360,0:720,0:75]
+
+for i in range(137):
+    for j in range(75):
+        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
+
+udata[:,:,:,75:150]=u1
+        
+    
+ds = nc.Dataset('wind_orig_20000107080000.nc')
+u1=ds['u'][0:137,0:360,0:720,0:75]
+
+for i in range(137):
+    for j in range(75):
+        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
+
+udata[:,:,:,150:]=u1    
+
+
+np.save('normalized_train_u_0_50',udata[0:50,])
+np.save('normalized_train_u_50_137',udata[50:137,])
 
 ###
 
 
-udata=np.zeros((50,360,720,75*4),dtype=np.float32)
+udata=np.zeros((137,360,720,75*3),dtype=np.float32)
 
-ds = nc.Dataset('../download_era5_data/train_u.nc')
-u1=ds['u'][0:50,0:360,0:720,0:75*4]
+ds = nc.Dataset('wind_orig_20000101000000.nc')
+u1=ds['v'][0:137,0:360,0:720,0:75]
 
-for i in range(50):
+for i in range(137):
     for j in range(75):
         u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
 
 udata[:,:,:,0:75]=u1
 
-np.save('normalized_train_u_0_50',udata)
+ds = nc.Dataset('wind_orig_20000104050000.nc')
+u1=ds['v'][0:137,0:360,0:720,0:75]
+
+for i in range(137):
+    for j in range(75):
+        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
+
+udata[:,:,:,75:150]=u1
+        
+    
+ds = nc.Dataset('wind_orig_20000107080000.nc')
+u1=ds['v'][0:137,0:360,0:720,0:75]
+
+for i in range(137):
+    for j in range(75):
+        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
+
+udata[:,:,:,150:]=u1    
+
+
+np.save('normalized_train_v_0_50',udata[0:50,])
+np.save('normalized_train_v_50_137',udata[50:137,])
+
 
 ###
 
-udata=np.zeros((137,360,720,10),dtype=np.float32)
-
-ds = nc.Dataset('../download_era5_data/eval_u.nc')
+ds = nc.Dataset('wind_orig_20000113140000.nc')
 u1=ds['u'][0:137,0:360,0:720,0:10]
-
 for i in range(137):
     for j in range(10):
         u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
-
-udata[:,:,:,0:75]=u1
-
+        
 np.save('normalized_val_u',udata)
 
 ###
 
-vdata=np.zeros((87,360,720,75*4),dtype=np.float32)
-
-ds = nc.Dataset('../download_era5_data/train_v.nc')
-u1=ds['v'][50:137,0:360,0:720,0:75*4]
-
-for i in range(87):
-    for j in range(75):
-        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
-
-udata[:,:,:,0:75]=u1
-
-np.save('normalized_train_v_50_137',udata)
-
-
-###
-
-vdata=np.zeros((50,360,720,75*4),dtype=np.float32)
-
-ds = nc.Dataset('../download_era5_data/train_v.nc')
-u1=ds['v'][0:50,0:360,0:720,0:75*4]
-
-for i in range(50):
-    for j in range(75):
-        u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
-
-udata[:,:,:,0:75]=u1
-
-np.save('normalized_train_v_0_50',udata)
-
-###
-
-udata=np.zeros((137,360,720,10),dtype=np.float32)
-
-ds = nc.Dataset('../download_era5_data/eval_v.nc')
+ds = nc.Dataset('wind_orig_20000113140000.nc')
 u1=ds['v'][0:137,0:360,0:720,0:10]
-
 for i in range(137):
     for j in range(10):
         u1[i,:,:,j]=mynormalize(u1[i,:,:,j])
-
-udata[:,:,:,0:75]=u1
-
+        
 np.save('normalized_val_v',udata)
