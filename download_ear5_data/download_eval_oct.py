11fdc857881b06857d006d40f0100f291651fa40
RudigerBrecht <rbrecht@mun.ca>
2022-10-10 15:13:02 +0200

    Add files via upload

---
+++ download_ear5_data/download_eval_oct.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import cdsapi
+c = cdsapi.Client()
+c.retrieve('reanalysis-era5-complete', {
+     'class': 'ea',
+     'date': '2000-10-14/to/2000-10-16',
+     'expver': '1',
+     'levelist': '1/to/137',
+     'levtype': 'ml',
+     'param': '155',
+     'stream': 'oper',
+     'time': [
+            '00:00', '01:00', '02:00',
+            '03:00', '04:00', '05:00',
+            '06:00', '07:00', '08:00',
+            '09:00', '10:00', '11:00',
+            '12:00', '13:00', '14:00',
+            '15:00', '16:00', '17:00',
+            '18:00', '19:00', '20:00',
+            '21:00', '22:00', '23:00',
+        ],
+     'type': 'an',
+     'format': 'grib'
+}, 'data_eval_oct.grib')
